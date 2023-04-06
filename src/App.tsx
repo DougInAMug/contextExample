5f9afa7ab8db2d35ccf67dba0568d05889548bd7
Doug <dmhwebb@gmail.com>
2023-04-06 15:20:43 +0200

    further minimize

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <h1>Hello World</h1>
